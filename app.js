3cf1620c98f7427a921211c8f8b1e28400c15f4a
Fatma Nur Karaman <fatmanur.karaman@outlook.com>
2023-04-23 12:01:57 +0300

    pcat project
    pcat project

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
-const fn = function () {
-    console.log('function')
-}
+const express = require('express')
+
+const app = express()
+
+app.get('/',(req,res)=>{
+
+    const photo={
+        id:1,
+        name:"photo name",
+        description:"photo desc"
+    }
+    res.send(photo.name)
+})
+
+const port = 3000
+app.listen(port, () => {
+    console.log(`sunucu ${port} portunda başladı`)
+})
