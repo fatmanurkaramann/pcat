2dd781ae15a955722b071581f5a755a34680e57f
Fatma Nur Karaman <fatmanur.karaman@outlook.com>
2023-04-29 00:43:06 +0300

    MVC Controller is added

--- app.js
+++ app.js
@@ -3,9 +3,8 @@ const mongoose = require('mongoose')
 const methodOverride = require('method-override')
 const fileUpload = require('express-fileupload');
 const ejs = require('ejs')
-const path = require('path')
-const fs = require('fs')
-const Photo = require('./models/Photo')
+const photoController = require('./controllers/photoControllers')
+const pageController = require('./controllers/pageController')
 
 const app = express();
 
@@ -21,72 +20,26 @@ app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(fileUpload());
-app.use(methodOverride('_method',{
-    methods:['POST','GET']
+app.use(methodOverride('_method', {
+    methods: ['POST', 'GET']
 }))
 
-app.get('/', async (req, res) => {
-    const photos = await Photo.find().sort('-dateCreated')
-    res.render('index', {
-        photos: photos
-    })
-})
 
-app.get('/about', (req, res) => {
-    res.render('about')
-})
-app.get('/add-photo', (req, res) => {
-    res.render('add')
-})
+app.get('/about', pageController.getAboutPage)
+app.get('/add-photo', pageController.getAddPage)
 
-app.get('/photos/:id', async (req, res) => {
-    //console.log(req.params.id)
-    const photo = await Photo.findById(req.params.id)
-    res.render('photoDetail', { photo })
-})
-app.post('/photos', async (req, res) => {
+app.get('/', photoController.getAllPhotos)
 
-    const uploadDir='public/uploads'
-    if(!fs.existsSync(uploadDir)){
-        fs.mkdirSync(uploadDir)
-    }
-    let uploadedImage=req.files.image
-    let uploadPath=__dirname+'/public/uploads/'+ uploadedImage.name
+app.get('/photos/:id', photoController.getPhoto)
 
-    uploadedImage.mv(uploadPath,
-        async ()=>{
-            await Photo.create({...req.body,
-            image:'/uploads/'+ uploadedImage.name
-            })
-        } //Bu geri çağrı fonksiyonu genellikle async/await yapısı kullanılarak yazılır ve taşıma işlemi tamamlanmadan önce diğer işlemlerin devam etmesini engeller.
-        )
+app.post('/photos', photoController.createPhoto)
 
-  
-    res.redirect('/')
-})
-app.get('/photos/edit/:id',async (req, res) => {
-    const photo = await Photo.findById(req.params.id)
-    res.render('edit',{photo})
-})
-app.put('/photos/:id',async(req,res)=>{
-    const photo=await Photo.findOne({_id:req.params.id})
-    await Photo.findByIdAndUpdate(photo, {
-        title: req.body.title,
-        description:req.body.description
-    })
-    photo.save()
-    res.redirect(`/photos/${req.params.id}`)
-})
-app.delete('/photos/:id',async(req,res)=>{
-    const photo=await Photo.findOne({_id:req.params.id})
-    let deletedImage=__dirname +'/public'+photo.image
+app.put('/photos/:id', photoController.updatePhoto)
 
-    fs.unlinkSync(deletedImage)
+app.delete('/photos/:id', photoController.deletePhoto)
 
-    await Photo.findByIdAndRemove(req.params.id)
-    
-    res.redirect('/')
-})
+
+app.get('/photos/edit/:id', pageController.getEditPage)
 const port = 3000
 app.listen(port, () => {
     console.log(`sunucu ${port} portunda başladı`)
