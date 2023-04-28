431c68d180d24fbffc014418b63106bb8c594c78
Fatma Nur Karaman <fatmanur.karaman@outlook.com>
2023-04-28 12:43:59 +0300

    photo update is done

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const methodOverride = require('method-override')
 const fileUpload = require('express-fileupload');
 const ejs = require('ejs')
 const path = require('path')
@@ -20,6 +21,7 @@ app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(fileUpload());
+app.use(methodOverride('_method'))
 
 app.get('/', async (req, res) => {
     const photos = await Photo.find().sort('-dateCreated')
@@ -34,6 +36,7 @@ app.get('/about', (req, res) => {
 app.get('/add-photo', (req, res) => {
     res.render('add')
 })
+
 app.get('/photos/:id', async (req, res) => {
     //console.log(req.params.id)
     const photo = await Photo.findById(req.params.id)
@@ -59,7 +62,19 @@ app.post('/photos', async (req, res) => {
   
     res.redirect('/')
 })
-
+app.get('/photos/edit/:id',async (req, res) => {
+    const photo = await Photo.findById(req.params.id)
+    res.render('edit',{photo})
+})
+app.put('/photos/:id',async(req,res)=>{
+    const photo=await Photo.findOne({_id:req.params.id})
+    await Photo.findByIdAndUpdate(photo, {
+        title: req.body.title,
+        description:req.body.description
+    })
+    photo.save()
+    res.redirect(`/photos/${req.params.id}`)
+})
 const port = 3000
 app.listen(port, () => {
     console.log(`sunucu ${port} portunda başladı`)
